f314ebb54061d9d2cd0a92468f9bd5d9128ec909
Rickie Alexander Bisnath <rabisnath@electrode.cs.uchicago.edu>
2018-08-01 16:57:09 -0500

    Working on custom images

--- Lab4/main.py
+++ Lab4/main.py
@@ -4,12 +4,15 @@
 
 # Imports the turtle graphics module
 import turtle
+import math
+from random import randint
  
 # creates a turtle (pen) an sets the speed (where 0 is fastest and 10 is slowest)
 # The colors can be set through their names or through hexadecimal codes, use hex for accuracy
 myPen = turtle.Turtle()
-myPen.speed(0)
+myPen.speed(10)
 myPen.color("#000000")
+turtle.delay(20)
 
 # setting out box sizes to the n sq pixels per box
 boxSize = 10
@@ -52,9 +55,64 @@ def box(intDim):
 
 # Challenge functions (2 bonus pts each) 
 # You need to make shapes with each
-#def circle(intRadius):
+def circle(intRadius):
+	myPen.begin_fill()
+	myPen.penup()
+	myPen.setheading(0)
+	myPen.forward(intRadius)
+	myPen.setheading(90)
+	myPen.pendown()
+	n = intRadius #number of sides in our polygon approximation of a circle
+	theta = (2 * math.pi) / float(n) #the external angle of the polygon
+	distance = math.sqrt( (2*(intRadius**2)) * (1 - math.cos(theta)) ) #side length
+	for i in range(int(n)):
+		myPen.forward(distance)
+		myPen.left(theta * (180 / math.pi))
+	myPen.end_fill()
+	myPen.penup()
+	myPen.setheading(0)
+
 #def triangle(intLength): #This can be an equilateral triangle
+
 #def save_image(): # saves the screen to an image/vector file
+
+def dec_to_hex(x):
+	numerals = '0123456789ABCDEF'
+	sixteens = int(math.floor(x/16))
+	ones = x % 16
+	a = numerals[sixteens]
+	b = numerals[ones]
+	return a + b
+
+#def draw_fractal_tree(branching_factor, shrinking_factor, theta, scale, levels):
+#	r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+#	r, g, b = dec_to_hex(r), dec_to_hex(g), dec_to_hex(b)
+#	myPen.color("#" + r + g + b)
+#	myPen.pendown()
+#	myPen.forward(scale)
+
+#	draw_branch(branching_factor, shrinking_factor, theta, scale, levels - 1, r, g, b)
+
+
+#def draw_branch(branching_factor, shrinking_factor, theta, scale, levels, r, g, b):
+
+#	for i in range(levels):
+		
+#		myPen.left(theta * math.floor(branching_factor / 2))
+
+#		for j in range(branching_factor):
+			#change color here maybe
+#			myPen.forward(scale)
+#			draw_branch(branching_factor, shrinking_factor, theta, scale, levels - 1, r, g, b)
+#			myPen.penup()
+#			myPen.forward(-1*scale)
+#			myPen.right(theta)
+#			myPen.pendown()
+
+#		myPen.left(theta * branching_factor / 2)
+		#myPen.forward(-1*scale)
+#		scale *= shrinking_factor
+#		theta *= 1 + (1 / levels)
 
 # These are the instructions on how to move "myPen" around after drawing a box.
 # penup() lifts the pen so it doesn't draw anything and can be moved freely
@@ -62,6 +120,24 @@ def box(intDim):
 # myPen.penup()
 # myPen.forward(boxSize)
 # myPen.pendown()
+
+def draw_fractal_tree(branching_factor, theta, size, shrinking_factor):
+	if size > 10:
+		myPen.left(theta * ((branching_factor / 2) ))
+		for i in range(branching_factor):
+			myPen.pendown()
+			myPen.forward(size)
+			draw_fractal_tree(branching_factor, theta*(0.75), size * shrinking_factor, shrinking_factor)
+			myPen.forward(-1 * size)
+			myPen.right(theta)
+
+			myPen.penup()
+			
+		#myPen.right(theta * ((branching_factor / 2) ))
+
+
+			
+
  
 # You will save your drawings as lists.
 # This first list stores the color values
@@ -211,7 +287,13 @@ def main():
 	#draw(pallet_3, pixels_3)
 	#draw(pallet_4, pixels_4)
 	#draw(pallet_5, pixels_5)
-	draw(pallet_6, pixels_6)
+	#draw(pallet_6, pixels_6)
+	#myPen.color("#DD00FF")
+	#circle(400)
+	#draw_fractal_tree(branching_factor, shrinking_factor, theta, scale, levels)
+	#draw_fractal_tree(3, 0.5, 15, 150, 3)
+	#draw_fractal_tree(branching_factor, theta, size, shrinking_factor)
+	draw_fractal_tree(2, 30, 150, 0.5)
 	# You need this to prevent the window from closing after drawing
 	turtle.done()
 
